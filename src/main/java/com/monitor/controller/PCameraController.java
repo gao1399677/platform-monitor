10ac3e15fe8139051c1a722c0f996d0179c461b6
gaosheng <gao.shengsheng@qq.com>
2021-10-11 22:47:40 +0800

    generate entity

---
+++ src/main/java/com/monitor/controller/PCameraController.java
@@ -0,0 +1,15 @@
+package com.monitor.controller;
+
+import com.monitor.service.PCameraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("camera")
+public class PCameraController {
+    @Autowired
+    private PCameraService pCameraService;
+
+    pu
+}
